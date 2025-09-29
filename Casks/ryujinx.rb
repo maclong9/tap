96c15d8af0328be10d33e8ac7aff2ba96519a21b
Mac Long <hello@maclong.uk>
2025-09-29 12:13:35 +0200

    Correct rpcs3 cask

--- Casks/ryujinx.rb
+++ Casks/ryujinx.rb
@@ -1,5 +1,6 @@
 cask "ryujinx" do
   version "1.3.2"
+  sha256 "c157907318348999ce4fb8c8fe8ddd511f0ddea643f22b1529527390180cb700"
 
   url "https://git.ryujinx.app/api/v4/projects/1/packages/generic/Ryubing/#{version}/ryujinx-#{version}-macos_universal.app.tar.gz"
   name "Ryujinx"
