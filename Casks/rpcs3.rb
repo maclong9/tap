bb2a93664ad9bcbe2a83ecd9c2e2d4fc348a82a9
Mac Long <hello@maclong.uk>
2025-09-30 16:05:10 +0200

    Update casks for rpcs3 to use x64 version for perf

--- Casks/rpcs3.rb
+++ Casks/rpcs3.rb
@@ -1,8 +1,8 @@
 cask "rpcs3" do
   version "v0.0.37-18159"
-  sha256 "771A43B1736B420E3E7B9921C5F72A633F38049A6212428FFD385824BC25DAC0" 
+  sha256 "73696A44D4477C184BAF41A9DB69448DF417657C3C099EB144BB5ACBEEDAB91F" 
 
-  url "https://github.com/RPCS3/rpcs3-binaries-mac-arm64/releases/download/build-6fe6c4e0f62d941a7056fcef5cb44d2339d725ed/rpcs3-v0.0.37-18159-6fe6c4e0_macos_arm64.7z",
+  url "https://github.com/RPCS3/rpcs3-binaries-mac/releases/download/build-6fe6c4e0f62d941a7056fcef5cb44d2339d725ed/rpcs3-v0.0.37-18159-6fe6c4e0_macos.7z",
       verified: "github.com/RPCS3/"
   name "RPCS3"
   desc "Open-source PlayStation 3 emulator"
