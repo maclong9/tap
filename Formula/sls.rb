0b75799d8c335efecc8caa41d248cd97a9055d81
Mac Long <hello@maclong.uk>
2025-11-13 13:02:45 +0100

    Add Linux builds for sls

--- Formula/sls.rb
+++ Formula/sls.rb
@@ -4,21 +4,34 @@ class Sls < Formula
   version "1.3.0"
   license "MIT"
 
-  on_arm do
-    url "https://github.com/maclong9/list/releases/download/v1.3.0/sls-aarch64"
-    sha256 "4f067d2030e60683723e2976f800ce492008d85d883b80f40161cb68097452d8"
+  on_macos do
+    on_arm do
+      url "https://github.com/maclong9/list/releases/download/v1.3.0/sls-aarch64"
+      sha256 "4f067d2030e60683723e2976f800ce492008d85d883b80f40161cb68097452d8"
+    end
+
+    on_intel do
+      url "https://github.com/maclong9/list/releases/download/v1.3.0/sls-x86_64"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    end
   end
 
-  on_intel do
-    url "https://github.com/maclong9/list/releases/download/v1.3.0/sls-x86_64"
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  on_linux do
+    on_intel do
+      url "https://github.com/maclong9/list/releases/download/v1.3.0/sls-linux-x86_64"
+      sha256 "786787d3dc85d553b82718adfc837a61f043e83420f27270d45f8079a68a8caa"
+    end
   end
 
   def install
-    if Hardware::CPU.arm?
-      bin.install "sls-aarch64" => "sls"
-    else
-      bin.install "sls-x86_64" => "sls"
+    if OS.mac?
+      if Hardware::CPU.arm?
+        bin.install "sls-aarch64" => "sls"
+      else
+        bin.install "sls-x86_64" => "sls"
+      end
+    elsif OS.linux?
+      bin.install "sls-linux-x86_64" => "sls"
     end
   end
 
